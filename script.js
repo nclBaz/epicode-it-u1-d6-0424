f3ac9376ecd4a53502ad4990405b2ab987ed3472
Riccardo Gulin <riccardo.gulin@gmail.com>
2024-05-06 12:01:19 +0200

    Adds data types examples

--- script.js
+++ script.js
@@ -39,3 +39,18 @@ const myName = "Riccardo"
 // myName = "Ajeje" // ERRORE! Il codice qua si rompe perché stiamo cercando di assegnare nuovamente un valore a qualcosa che invece è stato dichiarato come COSTANTE! è praticamente una scatola chiusa col nastro adesivo, sigillata!
 // Quindi è BUONA NORMA usare CONST un po' dappertutto per prevenire eventuali sovrascritture fatte per errore. Nella gran maggioranza dei casi ci troveremo a definire un valore all'inizio per poi non modificarlo più quindi è più probabile che utilizzeremo const più spesso che let. Se ci trovassimo in una situazione in cui invece effettivamente il valore deve cambiare per qualche ragione allora in tal caso utilizzeremo LET
 console.log("Il mio nome è: ", myName)
+
+const stringa1 = "Ciao "
+const stringa2 = "A Tutti"
+console.log(stringa1 + stringa2) // -> Concatena 2 stringhe
+console.log(stringa1 - stringa2) // -> NaN (Not A Number)
+console.log(stringa1 * stringa2) // -> NaN (Not A Number)
+console.log(stringa1 / stringa2) // -> NaN (Not A Number)
+
+// ***************************************** NULL E UNDEFINED ******************************
+let nonDefinita
+console.log("Il valore della variabile non definita è:", nonDefinita) // UNDEFINED è una variabile o non definita o alla quale non è stato assegnato valore
+let niente = null
+console.log("Il valore della variabile niente è:", niente) // NULL invece è una maniera volontaria di ANNULLARE il contenuto di una variabile
+niente = "Qualcosa"
+console.log("Il valore della variabile niente è:", niente)
